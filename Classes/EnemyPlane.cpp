3f9968723f7b2aa8e497d0f1a3fe243217db4009
Vitaly Nikolaev <vit3011@rambler.ru>
2015-11-21 13:35:38 +0300

    Added new class

---
+++ Classes/EnemyPlane.cpp
@@ -0,0 +1,9 @@
+//
+//  EnemyPlane.cpp
+//  FlightX
+//
+//  Created by Виталий on 21.11.15.
+//
+//
+
+#include "EnemyPlane.hpp"
---
+++ Classes/EnemyPlane.hpp
@@ -0,0 +1,14 @@
+//
+//  EnemyPlane.hpp
+//  FlightX
+//
+//  Created by Виталий on 21.11.15.
+//
+//
+
+#ifndef EnemyPlane_hpp
+#define EnemyPlane_hpp
+
+#include <stdio.h>
+
+#endif /* EnemyPlane_hpp */
